b058e4fc6463648d5e696d9c4baa28f06c3d8e0a
Simon Viart <simonviart59@gmail.com>
2023-07-11 14:20:01 +0200

    mongodb added + dummy data fetched

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Link from "next/link";
 import UnComp from "../components/UnComp";
+import { MongoClient } from "mongodb";
 
 async function getOneTodo() {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
  
   return res.json();
+}
+
+async function getAnything() {
+  const client = await MongoClient.connect(process.env.ATLAS_URI);
+  const db = client.db();
+
+  const svCollec = db.collection("sv_test_collection");
+
+  //const result = await svCollec.insertOne({title:'un troisieme test',num:8899999});
+  const result = await svCollec.find().toArray();
+
+  client.close();
+  
+  return result;
 }
 
 async function getAllTodos() {
@@ -23,6 +38,9 @@ export default async function Page() {
     title:string
   } = await getOneTodo(); 
 
+  const test = await getAnything();
+  //const test = 'hey';
+
   //const allTodos: number[] = await getAllTodos(); 
 
   return (
@@ -32,6 +50,8 @@ export default async function Page() {
       <li><Link href={`/todo/1`}>test</Link></li>
         {/* allTodos.map(todo => <li><Link href={`/todo/${todo}`}>{todo}</Link></li>) */}
       </ul>
+      {test.map((untest, index) => <div key={index}>{untest.title} avec le num {untest.num}</div>)}
     </>
   );
 }
+
